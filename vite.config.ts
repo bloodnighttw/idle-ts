93501e884cd3f7a9c9e0778b631939571c36e081
bloodnighttw <bbeenn1227@gmail.com>
2025-03-26 03:18:16 +0800

    feat: add base url.

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+    base: "/idle-ts/",
     resolve: {
         alias: {
             '@': resolve(__dirname, './src')
-        }
+        },
     }
 });
